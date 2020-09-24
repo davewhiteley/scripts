4310a4f8973a5ccd57dc0529f65b0abdb2991169
David Whiteley <David.Whiteley@q2ebanking.com>
2020-09-24 15:04:39 -0500

    Add git.sh

---
+++ git.sh
@@ -0,0 +1,8 @@
+# This is a collection of git-related commands and snippets
+
+# Prevent users from accidentally running this entire file as a script
+exit 0
+
+# Undo the act of committing
+# See also: https://stackoverflow.com/questions/2845731/how-to-uncommit-my-last-commit-in-git
+git reset --soft HEAD^
