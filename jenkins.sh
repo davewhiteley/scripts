4f1add91ee39ecca75cafa1d8edd9fa25124ee09
David Whiteley <davewhiteley@users.noreply.github.com>
2022-01-13 10:18:11 -0600

    Jenkins - add plugin list

--- jenkins.sh
+++ jenkins.sh
@@ -1,10 +1,16 @@
-# This is a collection of Jenkins-related commands and snippets
+# This is a collection of Jenkins-related commands and snippets.
+# Navigate to the Jenkins Script Console <Jenkins URL>/script to run these.
 
 # Prevent users from accidentally running this entire file as a script
 exit 0
 
-# Decrypt / "unencrypt" stored credentials
+# Get a list of installed plugins in shortName:version format (good for plugins.txt)
+Jenkins.instance.pluginManager.plugins.each{
+  plugin -> 
+    println ("${plugin.getShortName()}:${plugin.getVersion()}")
+}
+
+# Decrypt stored credentials
 # https://devops.stackexchange.com/questions/2191/how-to-decrypt-jenkins-passwords-from-credentials-xml
-# Navigate to the Jenkins Script Console <Jenkins URL>/script
-println(hudson.util.Secret.decrypt("{XXX=}"))
 # replacing {XXX=} with the credential
+println(hudson.util.Secret.decrypt("{XXX=}"))
