40b8d4614ca0dd4517d01bed858d00236c9b6487
David Whiteley <davewhiteley@users.noreply.github.com>
2022-02-02 09:39:16 -0600

    Jenkins - add command for finding large workspaces

--- jenkins.sh
+++ jenkins.sh
@@ -1,8 +1,11 @@
 # This is a collection of Jenkins-related commands and snippets.
-# Navigate to the Jenkins Script Console <Jenkins URL>/script to run these.
 
 # Prevent users from accidentally running this entire file as a script
 exit 0
+
+
+
+########## COMMANDS RUN IN THE JENKINS SCRIPT CONSOLE <Jenkins URL>/script ##########
 
 # Get a list of installed plugins in shortName:version format (good for plugins.txt)
 Jenkins.instance.pluginManager.plugins.each{
@@ -14,3 +17,10 @@ Jenkins.instance.pluginManager.plugins.each{
 # https://devops.stackexchange.com/questions/2191/how-to-decrypt-jenkins-passwords-from-credentials-xml
 # replacing {XXX=} with the credential
 println(hudson.util.Secret.decrypt("{XXX=}"))
+
+
+
+########## COMMANDS RUN ON JENKINS HOSTS ##########
+
+# Find large (> 1GB) workspaces on a build agent (note workspace path)
+du -hs /home/build/workspace/* | grep G
