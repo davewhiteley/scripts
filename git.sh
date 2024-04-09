d87abf461475990fe21a4f8d042709c5467d9289
David Whiteley <davewhiteley@users.noreply.github.com>
2024-04-09 15:59:33 -0500

    Git - add script for fast-forwarding a branch

--- git.sh
+++ git.sh
@@ -6,3 +6,9 @@ exit 0
 # Undo the act of committing
 # See also: https://stackoverflow.com/questions/2845731/how-to-uncommit-my-last-commit-in-git
 git reset --soft HEAD^
+
+# Fast-forward an existing branch
+TARGET_SHA='<The SHA to which you are moving the branch>'
+CURRENT_SHA='<The SHA that the branch is currently on>'
+BRANCH_NAME='<The name of the branch to be moved>'
+git rebase --onto "${TARGET_SHA} ${CURRENT_SHA} ${BRANCH_NAME}"
